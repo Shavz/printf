c79111c01815d3d9351a6d83e9b23dc022c16317
Shavz <104979137+Shavz@users.noreply.github.com>
2022-07-10 19:42:45 +0200

    Update print_hex.c

--- print_hex.c
+++ print_hex.c
@@ -5,3 +5,45 @@
 * @c: case of printing (0 = lower, 1 = upper)
 * Return: size of output
 */
+
+int print_hex(unsigned int n, unsigned int c)
+{
+	unsigned int len, powten, j, digit, num;
+	int count = 0;
+	char diff;
+
+	if (n != 0)
+	{
+		num = n;
+		len = 0;
+		if (c)
+			diff = 'A' - ':';
+		else
+			diff = 'a' - ':';
+		while (num != 0)
+		{
+			num /= 16;
+			len++;
+		}
+		powten = 1;
+		for (j = 1; j <= len - 1; j++)
+			powten *= 16;
+		for (j = 1; j <= len; j++)
+		{
+			digit = n / powten;
+			if (digit < 10)
+				_putchar(digit + '0');
+			else
+				_putchar(digit + '0' + diff);
+			count++;
+			n -= digit * powten;
+			powten /= 16;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
+}
