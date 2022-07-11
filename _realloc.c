b5065aae9a4b3872e65c569cb410ba0fef5b92de
Shavz <104979137+Shavz@users.noreply.github.com>
2022-07-11 21:42:57 +0200

    Create _realloc.c

---
+++ _realloc.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer
+ * @old_size: size of memory
+ * @new_size: new memory size
+ * Return: pointer to new mem
+ */
