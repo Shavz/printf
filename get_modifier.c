0987d179825f2d52829ac41f33d188e0dc1adb29
fredie7 <52379604+fredie7@users.noreply.github.com>
2022-07-11 20:59:26 +0100

    Update get_modifier.c

--- get_modifier.c
+++ get_modifier.c
@@ -12,3 +12,34 @@ void free_modifier(modifier_t *modif)
 	free(modif->length);
 	free(modif);
 }
+/**
+ * get_modifier - checks for specific mod to use
+ * @s: string
+ * @pos: position to start
+ * Return: pointer
+ */
+
+modifier_t *get_modifier(const char *s, unsigned int *pos)
+{
+	modifier_t *modif;
+	unsigned int i = *pos;
+
+	if (s[i + 1] == '\0')
+		return (NULL);
+	modif = malloc(sizeof(modifier_t));
+	if (modif == NULL)
+		return (NULL);
+	modif->flags = get_flags(s, &i);
+	modif->width = get_width(s, &i);
+	modif->precision = get_precision(s, &i);
+	modif->length = get_length(s, &i);
+	modif->specifier = get_specifier(s, &i);
+
+	if (!modif->specifier)
+	{
+		free_modifier(modif);
+		return (NULL);
+	}
+	(*pos) = i;
+	return (modif);
+}
