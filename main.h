b9b1d520dc893498333e7cbd7bc28615165448e7
fredie7 <52379604+fredie7@users.noreply.github.com>
2022-07-11 21:15:16 +0100

    update main.h

--- main.h
+++ main.h
@@ -2,33 +2,70 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_string(va_list s);
-int print_int(va_list i);
-int print_dec(va_list d);
-int print_bin(va_list b);
-int print_unsig(va_list u);
-int print_octal(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_rev(va_list r);
-int print_rot13(va_list R);
+#define BUFFER_SIZE 1024
 
 /**
-  * struct code_format - Struct format
-  * @sc: The specifiers
-  * @f: The function associated
-  */
+ * struct modifier - modifier fields
+ * @flags: flags field
+ * @width: width field
+ * @precision: precision field
+ * @length: length field string
+ * @specifier: specifier character
+ */
 
-typedef struct code_format
+typedef struct modifier
 {
-	char *sc;
-	int (*f)(va_list);
-} code_f;
+	char *flags;
+	int width;
+	int precision;
+	char *length;
+	char specifier;
+} modifier_t;
+
+void *_realloc(void *ptr, unsigned int, unsigned int);
+int _putchar(char c);
+char *print_binary(modifier_t *, va_list);
+char *print_unsigned_int(modifier_t *, va_list);
+char *print_octal(modifier_t *, va_list);
+char *print_hex(modifier_t *, va_list);
+char *print_char(modifier_t *, va_list list);
+char *print_int(modifier_t *, va_list list);
+char *print_string(modifier_t *modif, va_list list);
+char *print_rev(modifier_t *, va_list list);
+char *print_big_s(modifier_t *, va_list);
+char *print_pointer(modifier_t *, va_list);
+char *rot13(char *s);
+int _strlen(char *s);
+char *print_rot(modifier_t *, va_list list);
+
+void free_modifier(modifier_t *);
+char *get_flags(const char *, unsigned int *);
+int get_width(const char *, unsigned int *);
+int get_precision(const char *, unsigned int *);
+char *get_length(const char *, unsigned int *);
+char get_specifier(const char *, unsigned int *);
+modifier_t *get_modifier(const char *, unsigned int *);
+char *treat_format(const char *, unsigned int *, va_list);
+int _printf(const char *format, ...);
+char *_strcpy(char *dest, char *src);
+void array_rev(char *arr, int len);
+int int_len(int num);
+char *ito(int n);
+char *reverse(char *s);
+
+/**
+ * struct print - multiple choice print
+ * @f: char
+ * @func: function
+ */
+
+typedef struct print
+{
+	char f;
+	char *(*func)(modifier_t *, va_list);
+} t_print;
 
 #endif
