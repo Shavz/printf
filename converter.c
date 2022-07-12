943a359fb65f3b2f17261cf797e65ddc4e97582e
fredie7 <52379604+fredie7@users.noreply.github.com>
2022-07-12 20:38:38 +0100

    Update converter.c

--- converter.c
+++ converter.c
@@ -18,9 +18,10 @@ char *convert(unsigned long int num, int base, int lowercase)
 : "0123456789ABCDEF";
     ptr = &buffer[49];
     *ptr = '\0';
-    do {
-            *--ptr = rep[num % base];
-            num /= base;
-       } while (num != 0);
+    do 
+    {
+    *--ptr = rep[num % base];
+    num /= base;
+    } while (num != 0);
     return (ptr);
 }
