cda810e1a9a1aa1d59fc26e433d2827d9b997150
fredie7 <52379604+fredie7@users.noreply.github.com>
2022-07-12 22:20:02 +0100

    Update print_nums.c

--- print_nums.c
+++ print_nums.c
@@ -11,7 +11,6 @@ int print_int(va_list l, flags_t *f)
 {
 int n = va_arg(l, int);
 int res = count_digit(n);
-
 if (f->space == 1 && f->plus == 0 && n >= 0)
 res += _putchar(' ');
 if (f->plus == 1 && n >= 0)
@@ -46,7 +45,6 @@ return (_puts(str));
 void print_number(int n)
 {
 unsigned int n1;
-
 if (n < 0)
 {
 _putchar('-');
